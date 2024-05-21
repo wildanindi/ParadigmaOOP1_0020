b389465663096f2deac06f710cff0abe41e2655e
wildanindi <wildanindi2@gmail.com>
2024-05-21 17:38:06 +0700

    menambah akses modifier public pada kelas

--- initialization.cpp
+++ initialization.cpp
@@ -8,4 +8,15 @@ class mahasiswa {
         const int id;
         string nama;
         float nilai;
-};
+
+    public:
+        mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai){
+            //definisi
+        }
+
+        ~mahasiswa(){
+            cout << "Id       = " << id << endl;
+            cout << "Nama     = " << nama << endl;
+            cout << "Nilai    = " << nilai << endl;
+        }
+};    
